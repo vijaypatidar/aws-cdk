032c30be8e660979d6573876d96a2ac27955f7a2
Vijay Patidar <vramotiya@gmail.com>
2023-06-23 10:56:39 +0530

    init

---
+++ lib/launch-templates/LaunchTemplateStack.ts
@@ -0,0 +1,25 @@
+import * as cdk from "aws-cdk-lib";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as iam from "aws-cdk-lib/aws-iam";
+import { Construct } from "constructs";
+
+export class AwsCdkStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const vpc = new ec2.Vpc(this, "VPC");
+
+    const sg1 = new ec2.SecurityGroup(this, "sg1", {
+      vpc: vpc,
+    });
+
+    const launchTemplate = new ec2.LaunchTemplate(this, "LaunchTemplate", {
+      machineImage: ec2.MachineImage.latestAmazonLinux(),
+      securityGroup: sg1,
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.T2,
+        ec2.InstanceSize.SMALL
+      ),
+    });
+  }
+}
